adf55d49d22e7a5dab88875e5af9ecc4d92abda9
Rodrigo Araujo <rodrigoleitearaujo777@gmail.com>
2022-03-23 19:36:34 -0300

    css variable colors to light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#344E41',
+        tertiary: '#A3B18A',
+
+        text: '#3A5A40',
+        background: '#DAD7CD'
+    }
+};
