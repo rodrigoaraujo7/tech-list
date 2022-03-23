080e8094af4eaa35f10509aabdf54432257622b6
Rodrigo Araujo <rodrigoleitearaujo777@gmail.com>
2022-03-23 19:58:20 -0300

    react-switch props

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-
+import Switch from 'react-switch';
 import { Container } from "./styles";
 
 const Header = () => {
     return(
         <Container>
-            Darkmode button
+            <Switch 
+                onChange={() => {}}
+                checked={false}
+                checkedIcon={false}
+                uncheckedIcon={false}
+                height={18}
+                width={50}
+                handleDiameter={28}
+                offColor=""
+                onColor=""
+            />
             <br />
             Search box
         </Container>
