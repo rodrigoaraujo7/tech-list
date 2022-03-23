65d919abb8e1a8bee33ed80041b3b8f8b8728b18
Rodrigo Araujo <rodrigoleitearaujo777@gmail.com>
2022-03-23 20:43:49 -0300

    theme swap local storage hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React from 'react'
+import usePersistedState from './utils/usePersistedState'
 
 import { ThemeProvider } from 'styled-components'
 import light from './styles/themes/light'
@@ -8,7 +9,7 @@ import GlobalStyle from './styles/global'
 import Header from './components/Header/Index'
 
 function App() {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState('theme', light);
 
   const toggleTheme = () => {
     setTheme(theme.title === 'light' ? dark : light);
