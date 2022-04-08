16d05419297b86d0b86f5ccf46827a910fcaa15b
Rodrigo Araujo <rodrigoleitearaujo777@gmail.com>
2022-04-08 20:54:28 -0300

    functionality buttons

--- src/components/TechnologiesList/Index.tsx
+++ src/components/TechnologiesList/Index.tsx
@@ -1,25 +1,27 @@
 import { ContainerGrid, CardTechnology, CenterContent, Button } from "./styles"
 import Data from '../../data.json'
 
+import { BrowserRouter as Router, Link } from "react-router-dom"
+
 export default function TechnologiesList() {
     return (
-        <>
-        <ContainerGrid>
-            { Data.map(technology => {
-                return (
-                    <CardTechnology>
-                        <CenterContent>
-                            <img
-                            style={{
-                                width: '130px',
-                                marginTop: '1.75rem'
-                            }}
-                            src={technology.image} />
-                            <Button>{technology.title}</Button>
-                        </CenterContent>  
-                    </CardTechnology>
-                )}) }
-        </ContainerGrid>
-        </> 
+        <Router>
+            <ContainerGrid>
+                { Data.map(technology => {
+                    return (
+                        <CardTechnology>
+                            <CenterContent>
+                                <img
+                                style={{
+                                    width: '130px',
+                                    marginTop: '1.75rem'
+                                }}
+                                src={technology.image} />
+                                <Button><Link to="/php">{technology.title}</Link></Button>
+                            </CenterContent>  
+                        </CardTechnology>
+                    )}) }
+            </ContainerGrid>
+        </Router> 
     )
 }
