1febe718d0258396caf9d4a0c6b5dc085f2c15ed
Rodrigo Araujo <rodrigoleitearaujo777@gmail.com>
2022-04-08 20:19:09 -0300

    error page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Php from './pages/Php';
+import ErrorPage from './pages/ErrorPage';
 
 export function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path='/php' element={<Php />} />
+                <Route path='*' element={<ErrorPage />} />
             </Routes>
         </Router>
     )
