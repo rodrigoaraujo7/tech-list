0f145421e13ebee590580020cf6dfd6d3049d3f6
Rodrigo Araujo <rodrigoleitearaujo777@gmail.com>
2022-04-10 20:57:45 -0300

    basic header on new page

--- src/pages/TechPage.tsx
+++ src/pages/TechPage.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Container, Box } from "./styles";
 
 export default function TechPage() {
     return (
-        <div>TECH PAGE</div>
+        <Container>
+            <div className="left" style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <img src="../icons/return-dark.png" />
+                <h2 style={{ marginLeft: '1rem' }}>Return</h2>
+            </div>
+            <div className="right">
+                <Box />
+            </div>
+        </Container>
     )
 }
---
+++ src/pages/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 5rem;
+    width: 100%;
+
+    margin: 4.75rem 0;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const Box = styled.div`
+    height: 1.25rem;
+    width: 2rem;
+
+    background: red;
+`
